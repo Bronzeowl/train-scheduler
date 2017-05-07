185c46e0ff55ea865dbe4b47c0a6dd8af05fd648
Marie Dagan <mdagan03@gmail.com>
2017-05-07 18:43:30 -0500

    changed firebase

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,40 +1,40 @@
-$( document ).ready(function() {
-  
-   // Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyAuPL6IaOIYpjJog5HLD7686hwgSTQi0jY",
-      authDomain: "hi-mom-19901.firebaseapp.com",
-      databaseURL: "https://hi-mom-19901.firebaseio.com",
-      storageBucket: "hi-mom-19901.appspot.com",
-      messagingSenderId: 634830218698
-    };
-    firebase.initializeApp(config);
+$(document).ready(function () {
 
-    var database = firebase.database();
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA4Y6Ywc3MMHCQ7tZHkMyH1l9vx_qSRA6M",
+    authDomain: "train-schedule-5f60e.firebaseapp.com",
+    databaseURL: "https://train-schedule-5f60e.firebaseio.com/",
+    storageBucket: "train-schedule-5f60e.appspot.com",
+    messagingSenderId: 287123784516
+  };
+  firebase.initializeApp(config);
 
-    // Capture Button Click
-    $("#addTrain").on("click", function() {
+  var database = firebase.database();
+
+  // Capture Button Click
+  $("#addTrain").on("click", function () {
     event.preventDefault();
 
-      // Grabbed values from text boxes
-      var trainName = $("#trainName").val().trim();
-      var destination = $("#destination").val().trim();
-      var firstTrain = $("#firstTrain").val().trim();
-      var freq = $("#interval").val().trim();
+    // Grabbed values from text boxes
+    var trainName = $("#trainName").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTrain = $("#firstTrain").val().trim();
+    var freq = $("#interval").val().trim();
 
-      // Code for handling the push
-      database.ref().push({
+    // Code for handling the push
+    database.ref().push({
       trainName: trainName,
       destination: destination,
       firstTrain: firstTrain,
       frequency: freq
-      });
     });
-    
-    
-    // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
-database.ref().on("child_added", function(childSnapshot) {
-    
+  });
+
+
+  // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+  database.ref().on("child_added", function (childSnapshot) {
+
     var newTrain = childSnapshot.val().trainName;
     var newLocation = childSnapshot.val().destination;
     var newFirstTrain = childSnapshot.val().firstTrain;
@@ -42,13 +42,13 @@ database.ref().on("child_added", function(childSnapshot) {
 
     // First Time (pushed back 1 year to make sure it comes before current time)
     var startTimeConverted = moment(newFirstTrain, "hh:mm").subtract(1, "years");
-  
+
     // Current Time
     var currentTime = moment();
 
     // Difference between the times
     var diffTime = moment().diff(moment(startTimeConverted), "minutes");
- 
+
     // Time apart (remainder)
     var tRemainder = diffTime % newFreq;
 
@@ -58,23 +58,23 @@ database.ref().on("child_added", function(childSnapshot) {
     // Next Train
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
     var catchTrain = moment(nextTrain).format("HH:mm");
-    
+
     // Display On Page
     $("#all-display").append(
-      ' <tr><td>' + newTrain + 
-      ' </td><td>' + newLocation + 
-      ' </td><td>' + newFreq + 
-      ' </td><td>' + catchTrain + 
-      ' </td><td>' + tMinutesTillTrain +  ' </td></tr>');
+      ' <tr><td>' + newTrain +
+      ' </td><td>' + newLocation +
+      ' </td><td>' + newFreq +
+      ' </td><td>' + catchTrain +
+      ' </td><td>' + tMinutesTillTrain + ' </td></tr>');
 
-      // Clear input fields
-    $("#trainName, #destination, #firstTrain, #interval").val(""); 
+    // Clear input fields
+    $("#trainName, #destination, #firstTrain, #interval").val("");
     return false;
-    },
+  },
     //Handle the errors
-    function(errorObject) {
-    console.log("Errors handled: " + errorObject.code);
+    function (errorObject) {
+      console.log("Errors handled: " + errorObject.code);
     });
-    
-  }); //end document ready
+
+}); //end document ready
 
